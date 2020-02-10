1b213f22c70131137de78d8c1cac08f167c3fa31
Jordan <jordan.heider@etkg.com>
2020-02-10 14:29:46 -0600

    add styles from external css

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -8,7 +8,7 @@ var About = (props) => {
   }
   return (
     <div>
-      <h1 className="text-center">About</h1>
+      <h1 className="text-center page-title">About</h1>
       <p>This is a weather application, built for The Complete React Web App Developer Course.</p>
       This app uses:
       <div className="grid-container">
